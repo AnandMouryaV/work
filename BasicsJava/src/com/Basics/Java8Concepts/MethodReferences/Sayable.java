c889256422a221476820fe5dd1214d2de57b41fe
AnandMourya <themourya.v@gmail.com>
2021-06-08 16:03:33 +0530

    Dynamic Port Changes

---
+++ BasicsJava/src/com/Basics/Java8Concepts/MethodReferences/Sayable.java
@@ -0,0 +1,8 @@
+package com.Basics.Java8Concepts.MethodReferences;
+
+@FunctionalInterface
+public interface Sayable {
+	
+	public void say(String message);
+
+}
--- BasicsJava/src/com/Basics/MultiThreading/Demo.java
+++ BasicsJava/src/com/Basics/MultiThreading/Demo.java
@@ -8,6 +8,9 @@ public class Demo {
 	
 	Thread t1 = new Thread(new Producer(service));
 	Thread t2 = new Thread(new Consumer(service));
+	ExampleClass ex = new ExampleClass();
+	
+	ex.start();
 	t1.start();
 	t2.start();
 	}
